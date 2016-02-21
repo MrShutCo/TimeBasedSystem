1ff093dfe72bdfcb98408f18ea1d0740b9459317
MrShutCo <tylersafaric99@gmail.com>
2016-02-21 10:48:05 -0500

    Basic Event System Added
    Able to add events, that last for a certain time, then are garbage
    collected

---
+++ TimeBasedSystem/TimeBasedSystem/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeBasedSystem {
+    public class Timer {
+
+        public int Time
+        {
+            get;
+            protected set;
+        }
+
+        public int TimeStep
+        {
+            get;
+            protected set;
+        }
+
+        public Timer(int timeStep = 1) {
+            Time = 0;
+            TimeStep = timeStep;
+        }
+
+        public void Update() {
+            Time += TimeStep;
+            Console.WriteLine("The current time is: " + Time);
+        }
+
+    }
+}
